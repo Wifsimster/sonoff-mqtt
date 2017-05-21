4e5f37bb5598033ec89d144618c2fca49ec7107d
Wifsimster <wifsimster@gmail.com>
2017-05-22 00:28:58 +0200

    Add online mode

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 require('config')
 
-TOPIC = "/sensors/relay/data"
+TOPIC = "/sensors/relay/"
 RELAY_PIN = 6
 BTN_PIN = 3
 DEBOUNCE = 250
@@ -12,6 +12,7 @@ gpio.mode(BTN_PIN, gpio.INPUT, gpio.PULLUP)
 gpio.mode(MQTT_LED, gpio.OUTPUT)
 gpio.write(MQTT_LED, gpio.HIGH)
 
+mac = wifi.sta.getmac()
 ip = wifi.sta.getip()
 m = mqtt.Client(CLIENT_ID, 120, "", "")
 
@@ -26,22 +27,30 @@ function mqtt_activity()
     end)
 end
 
--- Publish MQTT activity to broker
-function mqtt_update()
-    DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..ip..'", "state":"'..gpio.read(RELAY_PIN)..'"}'
-    m:publish(TOPIC, DATA, 0, 0, function(conn)
-        print(CLIENT_ID.." sending online: "..DATA.." to "..TOPIC)
+-- Say hello to broker
+function mqtt_online()
+    DATA = '{"mac":"'..mac..'","ip":"'..ip..'","state":"'..gpio.read(RELAY_PIN)..'","online":"true","type":"switch"}'
+    m:publish("/online/", DATA, 0, 0, function(conn)
+        print(CLIENT_ID.." : "..DATA.." to "..TOPIC)
     end)
 end
 
-m:lwt("/lwt", '{"message":"'..CLIENT_ID..'", "topic":"'..TOPIC..'", "ip":"'..ip..'"}', 0, 0)
+-- Publish MQTT activity to broker
+function mqtt_update()
+    DATA = '{"mac":"'..mac..'","ip":"'..ip..'","state":"'..gpio.read(RELAY_PIN)..'"}'
+    m:publish(TOPIC, DATA, 0, 0, function(conn)
+        print(CLIENT_ID.." : "..DATA.." to "..TOPIC)
+    end)
+end
+
+m:lwt("/lwt", '{"message":"'..CLIENT_ID..'","topic":"'..TOPIC..'","ip":"'..ip..'"}', 0, 0)
 
 -- Try to reconnect to broker when communication is down
 m:on("offline", function(con)
     ip = wifi.sta.getip()
     print ("MQTT reconnecting to " .. BROKER_IP .. " from " .. ip)
     tmr.alarm(1, 10000, 0, function()
-        node.restart();
+        node.restart()
     end)
 end)
 
@@ -68,17 +77,19 @@ m:on("message", function(conn, topic, data)
     mqtt_activity()
     print("Message received: " .. topic .. " : " .. data)
     parse = cjson.decode(data)
+    mac = parse.mac
     state = parse.state
-    if (state == "1") then
-        print("Relay enable")
-        gpio.write(RELAY_PIN, gpio.HIGH)
-    elseif (state == "0") then
-        print("Relay disable")
-        gpio.write(RELAY_PIN, gpio.LOW)
-    else
-        print("Invalid state (" .. state .. ")")
+    if(mac == wifi.sta.getmac()) then
+        if (state == "1") then
+            print("Relay enable")
+            gpio.write(RELAY_PIN, gpio.HIGH)
+        elseif (state == "0") then
+            print("Relay disable")
+            gpio.write(RELAY_PIN, gpio.LOW)
+        else
+            print("Invalid state (" .. state .. ")")
+        end
     end
-    --mqtt_update()
 end)
 
 -- Subscribe to MQTT broker
@@ -92,6 +103,7 @@ end
 print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
 m:connect(BROKER_IP, BROKER_PORT, 0, 1, function(conn)
     gpio.write(MQTT_LED, gpio.HIGH)
-    print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
-    mqtt_sub()
+    print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)    
+    mqtt_online()
+    mqtt_sub()  
 end)
